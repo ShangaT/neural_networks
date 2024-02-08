9393a2ad3d45fcb2e5e6b3bb27e2fa2c427339a9
ShangaT <shanyatad@yandex.ru>
2024-02-09 02:01:55 +0500

    Добавлено обучение

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import random
+import random, math
 
 class Element:
     def __init__(self, a_size):
@@ -8,16 +8,21 @@ class Element:
         self.g = 0
         self.d = 0
 
+    def sigmoid(self, x):
+        # Функция активации - сигмоид
+        return 1 / (1 + math.exp(-x))
+
     def calculation_y(self, layer_previous, layer_current):
         for i in range(len(layer_current)):
+            multiplication = 0
             for j in range(len(layer_previous)):                
-                multiplication = layer_previous[j].a[i] * layer_previous[j].y
-                layer_current[i].y += round(multiplication, 10)
+                multiplication += layer_previous[j].a[i] * layer_previous[j].y
+            layer_current[i].y = self.sigmoid(multiplication)        
         
     def calculation_last_g(self, layer):
         for i in range(len(layer)):
             g = layer[i].y * (1 - layer[i].y) * (layer[i].d - layer[i].y)
-            layer[i].g = round(g, 10)
+            layer[i].g = g
 
     def calculation_g(self, layer_previous, layer_current):
         for i in range(len(layer_current)):
@@ -25,13 +30,13 @@ class Element:
             for j in range(len(layer_previous)):
                 summa += layer_current[i].a[j] * layer_previous[j].g
         g = layer_current[i].y * (1 - layer_current[i].y) * summa
-        layer_current[i].g = round(g, 10)
+        layer_current[i].g = g
 
     def calculation_a(self, layer_previous, layer_current, r):
         for i in range(len(layer_current)):
             for j in range(len(layer_previous)):
                 a = layer_current[i].a[j] + layer_previous[j].g * layer_current[i].y * r
-                layer_current[i].a[j] = round(a, 10)
+                layer_current[i].a[j] = a
     
 class Perceptron(Element):
     def __init__(self, input_vector_size: int, input_size: int, hidden_size: int, output_size: int):
@@ -40,19 +45,19 @@ class Perceptron(Element):
         self.hidden_layer = [Element(output_size) for i in range(0, hidden_size)]
         self.output_layer = [Element(0) for i in range(0, output_size)]
 
-    def back_popagation(self, x, y, r):
+    def forward(self, x):
         #задаем входному слою значения y из значений входного вектора
         for i in range(len(x)): 
             self.input_vector[i].y = x[i]
-        #задаем выходному слою значения d из выходного вектора
-        for i in range(len(y)):
-            self.output_layer[i].d = y[i]
-
-        #while True:
-            #расчитываем y для осталььных - прямой проход
+        #расчитываем y для осталььных - прямой проход
         self.calculation_y(self.input_vector, self.input_layer)
         self.calculation_y(self.input_layer, self.hidden_layer)
         self.calculation_y(self.hidden_layer, self.output_layer)
+
+    def back_popagation(self, y, r):
+        #задаем выходному слою значения d из выходного вектора
+        for i in range(len(y)):
+            self.output_layer[i].d = y[i]
 
         self.calculation_last_g(self.output_layer) #расчитываем g для последнего слоя
 
@@ -66,51 +71,86 @@ class Perceptron(Element):
         self.calculation_g(self.input_layer, self.input_vector)
         self.calculation_a(self.input_layer, self.input_vector, r)
 
+    def lerning(self, x, y, r):
+        epohe = 0
+        while True:                        
+            for i in range(len(x)):
+                self.forward(x[i])
+                self.back_popagation(y[i], r)
+
+            delta = [] #отклонение
+            for i in range(len(y[0])):
+                delta.append(abs(self.output_layer[i].d - self.output_layer[i].y))
+                               
+            epohe += 1 #вывод кол-ва итераций
+            print('Кол-во эпох: ', epohe, end='\r')
+
+            if all(element <= 0.1 for element in delta): break
+            # if math.isnan(self.output_layer[0].d - self.output_layer[0].y): 
+            #     print('Ошибка')
+            #     break
+        print('Кол-во эпох: ', epohe)
+
     def predict(self, x):
         for i in range(len(x)): 
             self.input_vector[i].y = x[i]
+        self.forward(x)        
+        y = []
+        for i in self.output_layer:
+            y.append(i.y)
+        return y
 
-        self.calculation_y(self.input_vector, self.input_layer)
-        self.calculation_y(self.input_layer, self.hidden_layer)
-        self.calculation_y(self.hidden_layer, self.output_layer)
-        return self.output_layer[0].y
+if __name__ == '__main__':
+    x=[
+        [1, 0, 1,
+         0, 1, 0,
+         1, 0, 1],
+
+        [1, 1, 1,
+         1, 0, 1,
+         1, 1, 1],
+
+        [0, 0, 1,
+         0, 1, 0,
+         1, 0, 1],
+
+        [1, 0, 1,
+         1, 0, 1,
+         1, 1, 1],
+
+        [1, 0, 0,
+         0, 1, 0,
+         1, 0, 1],
+
+        [1, 1, 1,
+         1, 0, 0,
+         1, 1, 1],
+
+        [1, 0, 1,
+         0, 1, 0,
+         0, 0, 1],
         
-        #delta = [abs(element.d - element.y) for element in self.output_layer]
-            # for element in self.output_layer:
-            #     delta.append(abs(element.d - element.y))
+        [1, 1, 1,
+         0, 0, 1,
+         1, 1, 1]]
 
-            # for i in delta:
-            #     if i >= 0.5: break
-            # else: break
-            #if epohe == 0: break
+    y = [[1], [0], [1], [0], [1], [0], [1], [0]]
 
-x = [[1, 0, 1,
-      0, 1, 0,
-      1, 0, 1],
-     
-     [1, 1, 1,
-      1, 0, 1,
-      1, 1, 1],
-      
-      [1, 0, 0,
-       0, 1, 0,
-       1, 0, 1],
-      
-      [0, 1, 1,
-       1, 0, 1,
-       1, 1, 1]]
+    perseptron = Perceptron(len(x[0]), 4, 4, len(y[0]))
+    perseptron.lerning(x, y, r=0.1)
 
-y = [[1], [0], [1], [0]]
+    data_1=[1, 0, 1,
+            0, 1, 0,
+            1, 0, 0]
 
-perseptron = Perceptron(len(x[0]), 3, 3, len(y[0]))
+    data_2=[1, 1, 1,
+            1, 0, 1,
+            1, 0, 1]
+    
+    data_3=[1, 0, 1,
+            0, 0, 0,
+            1, 0, 1]
 
-while True:
-    for i in range(len(y)):
-        perseptron.back_popagation(x[i], y[i], r=0.1)
-        delta = abs(perseptron.output_layer[0].d - perseptron.output_layer[0].y)
-    if delta <= 0.2: break
-
-a = perseptron.predict([1,0,1,0,1,0,1,0,1])
-
-print(a)
-
+    print(f'ЗНАЧЕНИЕ 1: {perseptron.predict(data_1)}')
+    print(f'ЗНАЧЕНИЕ 2: {perseptron.predict(data_2)}')
+    print(f'ЗНАЧЕНИЕ 3: {perseptron.predict(data_3)}')
